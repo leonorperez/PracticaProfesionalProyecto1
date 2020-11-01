c338e22c350665d0f80fee0298bbc5bd10e5fe3f
Veronica Balsamo <veritobalsamo@gmail.com>
2020-11-01 19:31:18 -0300

    creación de tabla

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hola")
+window
+  .fetch("https://hp-api.herokuapp.com/api/characters")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const app = document.getElementById("app");
+    const table = document.createElement("table");
+    app.appendChild(table);
+    const trHead = document.createElement("tr");
+    const thNombre = document.createElement("th");
+    thNombre.innerText = "Nombre"; 
+    table.appendChild(trHead);
+    trHead.appendChild(thNombre);
+
+
+    for (let i = 0; i < data.length; i++) {
+      const tr = document.createElement("tr");
+      const td = document.createElement("td");
+      td.innerText = data[i].name;
+      tr.appendChild(td);
+      table.appendChild(tr);
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
