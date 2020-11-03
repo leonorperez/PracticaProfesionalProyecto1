fffd6ae50726f20b20e0f61ac560439f0c19abd5
leonorperez <leonzia_8@hotmail.com>
2020-11-02 21:51:09 -0300

    se crearon mas columnas de la tabla con algunos estilos

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
-var title = document.getElementById("title");
-
-title.innerHTML = "<h1>Harry !!</h1>"
-
-
-// const titulo = document.createElement("h1");
-// titulo.innerText = "Harry !!"
-// title.appendChild(titulo);
-
-
-
-
-
-
-
-
 window.fetch("https://hp-api.herokuapp.com/api/characters")
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
-        console.log(data)
         const app = document.getElementById("app");
         const table = document.createElement("table");
         app.appendChild(table);
         const trHead = document.createElement("tr");
         const thName = document.createElement("th");
-        const thSpecie = document.createElement("th");
         const thImage = document.createElement("th");
+        const thSpecie = document.createElement("th");
+        const thAncestry = document.createElement("th");
+        const thPatronus = document.createElement("th");
+        const thwand = document.createElement("th");
         thName.innerText = "Nombre";
         thSpecie.innerText = "Especie";
         thImage.innerText = "Imagen";
+        thAncestry.innerText = "Ancestros";
+        thPatronus.innerText = "Patronus";
+        thwand.innerText = "Varita";
         table.appendChild(trHead);
         trHead.appendChild(thName);
-        trHead.appendChild(thSpecie);
         trHead.appendChild(thImage);
+        trHead.appendChild(thSpecie);
+        trHead.appendChild(thAncestry);
+        trHead.appendChild(thPatronus);
+        trHead.appendChild(thwand);
 
 
 
@@ -42,12 +34,28 @@ window.fetch("https://hp-api.herokuapp.com/api/characters")
             const td = document.createElement("td");
             const td2 = document.createElement("td");
             const td3 = document.createElement("td");
+            const td4 = document.createElement("td");
+            const td5 = document.createElement("td");
+            const td6 = document.createElement("td");
+            console.log({ td3 })
+            td3.style.textAlign = "center"
             td.innerText = data[i].name;
-            td2.innerText = data[i].species
-            td3.innerHTML = `<img  style="width:70px;heigth:70px;" src="${data[i].image}" />`
+            td2.innerHTML = `<img  style="width:70px;heigth:70px;" src="${data[i].image}"/>`
+            td3.innerText = data[i].species;
+            td4.innerText = data[i].ancestry;
+            td5.innerText = data[i].patronus;
+            const ul = document.createElement("ul")
+            ul.innerHTML = `<li type="square">${ data[i].wand.core ? data[i].wand.core : '<img style="width:25px;border-radius:55%;" src="varitas.jpg" />'}</li>`
+            ul.innerHTML += `<li type="square">${ data[i].wand.length? data[i].wand.length : '<span style="color:red">X</span>'}</li>`
+            ul.innerHTML += `<li type="square">${ data[i].wand.wood ? data[i].wand.wood :'<span style="color:red">X</span>' }</li>`
+
+            td6.appendChild(ul);
             tr.appendChild(td);
-            tr.appendChild(td2);
-            tr.appendChild(td3)
+            tr.appendChild(td2)
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tr.appendChild(td6);
             table.appendChild(tr);
 
         }
