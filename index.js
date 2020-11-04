f38556802fa4ccd46d28992121e51b4b99d5ee57
Veronica Balsamo <veritobalsamo@gmail.com>
2020-11-04 01:27:18 -0300

    estilo a la tabla y columna casa

--- index.js
+++ index.js
@@ -5,23 +5,27 @@ window.fetch("https://hp-api.herokuapp.com/api/characters")
     .then(function(data) {
         const app = document.getElementById("app");
         const table = document.createElement("table");
+        table.className = "table table-dark";
         app.appendChild(table);
+        const thImage = document.createElement("th");
         const trHead = document.createElement("tr");
         const thName = document.createElement("th");
-        const thImage = document.createElement("th");
+        const thHouse = document.createElement("th")
         const thSpecie = document.createElement("th");
         const thAncestry = document.createElement("th");
         const thPatronus = document.createElement("th");
         const thwand = document.createElement("th");
-        thName.innerText = "Nombre";
-        thSpecie.innerText = "Especie";
         thImage.innerText = "Imagen";
+        thName.innerText = "Nombre";
+        thHouse.innerText ="Casa";
+        thSpecie.innerText = "Especie";
         thAncestry.innerText = "Ancestros";
         thPatronus.innerText = "Patronus";
         thwand.innerText = "Varita";
+        trHead.appendChild(thImage);
         table.appendChild(trHead);
         trHead.appendChild(thName);
-        trHead.appendChild(thImage);
+        trHead.appendChild(thHouse);
         trHead.appendChild(thSpecie);
         trHead.appendChild(thAncestry);
         trHead.appendChild(thPatronus);
@@ -30,31 +34,34 @@ window.fetch("https://hp-api.herokuapp.com/api/characters")
 
 
         for (let i = 0; i < data.length; i++) {
+            const tdImagen = document.createElement("td");
             const tr = document.createElement("tr");
-            const td = document.createElement("td");
-            const td2 = document.createElement("td");
-            const td3 = document.createElement("td");
-            const td4 = document.createElement("td");
-            const td5 = document.createElement("td");
-            const td6 = document.createElement("td");
+            const tdName = document.createElement("td");
+            const tdHouse = document.createElement("td");
+            const tdSpecies = document.createElement("td");
+            const tdAncestry = document.createElement("td");
+            const tdPatronus = document.createElement("td");
+            const tdVarita = document.createElement("td");
 
-            td.innerText = data[i].name;
-            td2.innerHTML = `<img  style="width:70px;heigth:70px;" src="${data[i].image}"/>`
-            td3.innerText = data[i].species;
-            td4.innerText = data[i].ancestry;
-            td5.innerText = data[i].patronus;
+            tdImagen.innerHTML = `<img  style="width:70px;heigth:70px;" src="${data[i].image}"/>`
+            tdName.innerText = data[i].name;
+            tdHouse.innerText = data[i].house;
+            tdSpecies.innerText = data[i].species;
+            tdAncestry.innerText = data[i].ancestry;
+            tdPatronus.innerText = data[i].patronus;
             const ul = document.createElement("ul")
             ul.innerHTML = `<li type="square">${ data[i].wand.core ? data[i].wand.core : '<img style="width:25px;border-radius:55%;" src="varitas.jpg" />'}</li>`
             ul.innerHTML += `<li type="square">${ data[i].wand.length? data[i].wand.length : '<span style="color:red">X</span>'}</li>`
             ul.innerHTML += `<li type="square">${ data[i].wand.wood ? data[i].wand.wood :'<span style="color:red">X</span>' }</li>`
 
-            td6.appendChild(ul);
-            tr.appendChild(td);
-            tr.appendChild(td2)
-            tr.appendChild(td3);
-            tr.appendChild(td4);
-            tr.appendChild(td5);
-            tr.appendChild(td6);
+            tdVarita.appendChild(ul);
+            tr.appendChild(tdImagen);
+            tr.appendChild(tdName);
+            tr.appendChild(tdHouse);
+            tr.appendChild(tdSpecies);
+            tr.appendChild(tdAncestry);
+            tr.appendChild(tdPatronus);
+            tr.appendChild(tdVarita);
             table.appendChild(tr);
 
         }
